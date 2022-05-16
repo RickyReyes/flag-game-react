6b6484e5bcfada999c55cba2096cfdb18e25e7b4
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2022-05-16 13:03:17 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import StartPage from './components/StartPage';
+import Choices from './components/Choices';
+import EndPage from './components/EndPage';
+import { useState, useEffect } from "react"
 
 function App() {
+  const [allCountries, setAllCountries] = useState([])
+  const [options, setOptions] = useState(null)
+  const [active, setActive] = useState(false)
+  const [correctIndex, setCorrectIndex] = useState(null)
+  const [numberCorrect, setNumberCorrect] = useState(0)
+  const GAME_LENGTH = 10
+  const [roundsLeft, setRoundsLeft] = useState(GAME_LENGTH)
+
+ useEffect(() => {
+    async function fetchCountries() {
+      const response = await fetch('https://restcountries.com/v3.1/all')
+      const data = await response.json()
+      setAllCountries(data)
+      setOptions(getOptions(data))
+    }
+    fetchCountries()
+  }, [])
+
+  function handleStart() {
+    setActive(true)
+  }
+
+  // /* take an array of (allCountries) and return an array of 5 random countries */
+  function getOptions(countriesArray) {
+    let newOptions = []
+    for (let i = 0; newOptions.length < 5; i++) {
+      let index = Math.floor(Math.random() * countriesArray.length)
+      if (!newOptions.includes(countriesArray[index])) {
+        newOptions.push(countriesArray[index]);
+      }
+    }
+    setCorrectIndex(Math.floor(Math.random() * 5))
+    return newOptions
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App text-center flex justify-center items-center w-full h-screen bg-gray-100">
+        
+        <div className="max-w-4xl flex flex-col items-center relative font-mono w-full h-full">
+          <header className="text-4xl font-bold text-indigo-500 my-4">
+          <h1>"flag game"</h1>
+          </header>
+
+          {/* loading circle */}
+          {allCountries.length == 0 && 
+            <div className="w-full h-full flex items-center justify-center">
+              <img className="w-10 h-10" src="https://raw.githubusercontent.com/Codelessly/FlutterLoadingGIFs/master/packages/cupertino_activity_indicator.gif" alt="loading circle animation" />
+            </div>
+          }
+
+          {allCountries.length > 0 && !active && 
+            <StartPage handleStart={handleStart} />}
+          {active && roundsLeft > 0 && <Choices options={options} 
+                        correctIndex={correctIndex}
+                        setCorrectIndex={setCorrectIndex}
+                        allCountries={allCountries} 
+                        setOptions={setOptions} 
+                        getOptions={getOptions} 
+                        numberCorrect={numberCorrect}
+                        setNumberCorrect={setNumberCorrect} 
+                        roundsLeft={roundsLeft}
+                        setRoundsLeft={setRoundsLeft}
+                        gameLength={GAME_LENGTH} />  
+          }
+
+          {roundsLeft == 0 && <EndPage gameLength={GAME_LENGTH}     numberCorrect={numberCorrect}
+          setNumberCorrect={setNumberCorrect}
+          setRoundsLeft={setRoundsLeft} />}
+          <p className="text-5xl absolute bottom-0 right-0 mx-4 my-2 text-indigo-500">ℝ®</p>
+        </div>
+      </div>
   );
 }
 
---
+++ src/components/Choices.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Choices({roundsLeft, setRoundsLeft, options, correctIndex, setCorrectIndex, setOptions, getOptions, allCountries, numberCorrect, setNumberCorrect, gameLength}) {
+
+    const [correctCountry, setCorrectCountry] = useState(options[correctIndex])
+    const [correctFlag, setCorrectFlag] = useState(correctCountry.flags.png)
+    const [optionElements, setOptionElements] = useState([])
+    
+    useEffect(() => {
+        setOptionElements(options.map(country => (
+            <button 
+                key={country.area} 
+                onClick={(e) => handleSelect(e)}
+                className="self-start text-lg font-bold py-1 px-2 mb-2 border border-indigo-500 rounded-full text-indigo-900 bg-indigo-100 hover:bg-indigo-200">
+                    {country.name.common}
+            </button>
+        )))
+    }, [roundsLeft]);
+
+    function handleSelect(e) {
+        e.target.disabled = true;
+        if (e.target.innerText == correctCountry.name.common) {
+            setNumberCorrect(prev => prev + 1)
+        }
+        let selectedOptionElements = options.map(country => (
+            <button 
+                key={country.area} 
+                onClick={(e) => handleSelect(e)}
+                className={"self-start text-lg font-bold py-1 px-2 mb-2 border border-indigo-500 rounded-full text-indigo-900 " + (country.name.common == correctCountry.name.common ? "bg-green-300" : "bg-red-300")}>
+                    {country.name.common}
+            </button>
+        ))
+        setOptionElements(selectedOptionElements)
+        setTimeout(() => {
+            setRoundsLeft(prev => prev - 1)
+            e.target.disabled = false;
+        }, 1000)
+    }
+
+    useEffect(() => {
+        setOptions(getOptions(allCountries))
+    }, [roundsLeft])
+  
+    useEffect(() => {
+        setCorrectIndex(Math.floor(Math.random() * 5))
+    }, [roundsLeft])
+
+    useEffect(() => {
+        setCorrectCountry(options[correctIndex])
+    }, [options])
+
+    useEffect(() => {
+        setCorrectFlag(options[correctIndex].flags.png)
+    }, [roundsLeft])
+
+    return (
+        <div className="h-full w-full flex flex-col items-center pt-4 px-2">
+            <div className="flex flex-col text-indigo-900 text-lg">
+                <img className="mb-4 h-48 w-auto shadow-xl" src={correctFlag} />
+                {optionElements}
+                <div>correct: {numberCorrect}/{gameLength-roundsLeft}</div>
+                <div>rounds left: {roundsLeft}</div>
+            </div>
+        </div>
+    )
+}
