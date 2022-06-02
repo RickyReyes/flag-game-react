ab7862494a671cff07a8b914de17a9af1a01730b
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2022-06-02 13:09:48 -0700

    refactor / flex-row on desktop

--- src/components/Choices.js
+++ src/components/Choices.js
@@ -18,22 +18,20 @@ export default function Choices({roundsLeft, setRoundsLeft, options, correctInde
     }, [roundsLeft]);
 
     function handleSelect(e) {
-        e.target.disabled = true;
         if (e.target.innerText == correctCountry.name.common) {
             setNumberCorrect(prev => prev + 1)
         }
         let selectedOptionElements = options.map(country => (
-            <button 
+            <button disabled
                 key={country.area} 
                 onClick={(e) => handleSelect(e)}
-                className={"self-start text-lg font-bold py-1 px-2 mb-2 border border-indigo-500 rounded-full text-indigo-900 " + (country.name.common == correctCountry.name.common ? "bg-green-300" : "bg-red-300")}>
+                className={"self-start text-lg font-bold py-1 px-2 mb-2 border border-indigo-500 rounded-full text-indigo-900 " + (country.name.common == correctCountry.name.common ? "bg-green-300" : e.target.innerText == country.name.common ? "bg-red-300" : "bg-indigo-200")}>
                     {country.name.common}
             </button>
         ))
         setOptionElements(selectedOptionElements)
         setTimeout(() => {
             setRoundsLeft(prev => prev - 1)
-            e.target.disabled = false;
         }, 1000)
     }
 
@@ -56,8 +54,12 @@ export default function Choices({roundsLeft, setRoundsLeft, options, correctInde
     return (
         <div className="h-full w-full flex flex-col items-center pt-4 px-2">
             <div className="flex flex-col text-indigo-900 text-lg">
-                <img className="mb-4 h-48 w-auto shadow-xl" src={correctFlag} />
-                {optionElements}
+                <div className="flex flex-col lg:flex-row lg:gap-8">
+                    <img className="mb-4 h-48 w-auto shadow-xl" src={correctFlag} />
+                    <div className="flex flex-col">
+                        {optionElements}
+                    </div>
+                </div>
                 <div>correct: {numberCorrect}/{gameLength-roundsLeft}</div>
                 <div>rounds left: {roundsLeft}</div>
             </div>
